298c1e284bb4d4abc32dab6ad36a9260fc328fbc
buruadam <adam.buru@gmail.com>
2024-11-17 17:33:27 +0100

    WIP

--- linter.py
+++ linter.py
@@ -1,9 +1,13 @@
 import sys
 from pylint import lint
-THRESHOLD = 8
-run = lint.Run(["helloworld.py"], do_exit=False)
+THRESHOLD = 10
+run = lint.Run(["helloworld.py"], exit=False)
+
 score = run.linter.stats.global_note
-if score < THRESHOLD:
-    print("Linter failed: Score < threshold value")
-    sys.exit(1)
-sys.exit(0)
+
+print(f"{score} pontot ért el a kódunk")
+
+if (score < THRESHOLD):
+	sys.exit(1)
+else:
+	sys.exit(0)
