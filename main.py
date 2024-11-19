13a04d7b3373d4ecdeea9fec41b9c321c5fcb828
buruadam <adam.buru@gmail.com>
2024-11-19 08:58:23 +0100

    update main

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-def helloworld():
-    print('Hello world')
+class BinarisKereses:
 
-helloworld()
+    def binary_search(arr, x):
+        low = 0
+        high = len(arr) - 1
+        while low <= high:
+            mid = (high + low) // 2
+            if arr[mid] < x:
+                low = mid + 1
+            elif arr[mid] > x:
+                high = mid - 1
+            else:
+                return mid
+        return -1
+    
+    arr = [2,3,4,10,40]
+    x = 10
+    result = binary_search(arr, x)
+    if result != -1:
+        print("Az elem a következő indexen van: ", str(result))
+    else:
+        print("Az elem nincs a tömbben!")
